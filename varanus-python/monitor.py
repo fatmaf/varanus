a92d43f395d2ced39f4494603bd3b65dfa5fcc97
Matt Luckcuck <m.luckcuck@liverpool.ac.uk>
2019-08-24 15:59:08 +0100

    Basic offline mode reading from trace.json

--- varanus-python/monitor.py
+++ varanus-python/monitor.py
@@ -1,13 +1,38 @@
 from fdr_interface import FDRInterface
+import json
 
-#"MASCOT_SAFETY_SYSTEM :[has trace]: <system_init>""
-#"csp/mascot-safety-system.csp
+#"MASCOT_SAFETY_SYSTEM :[has trace]: <system_init>"
+#"model/mascot-safety-system.csp"
 
 
-fdr = FDRInterface()
+class Monitor(object):
 
-fdr.load_model("model/mascot-safety-system.csp")
+    def __init__(self, model_path):
+        self.fdr = FDRInterface()
+        self.model_path = model_path
+        self.fdr.load_model(self.model_path)
 
-print fdr.check_trace(["system_init"])
+    def run_offline(self, log_path):
+        trace_file = open("trace.json")
 
-fdr.close()
+        for json_line in trace_file:
+            trace = json.loads(json_line)
+
+            result = self.fdr.check_trace(trace)
+            print result
+
+            if not result:
+                trace_file.close()
+                return result
+
+        return result
+
+
+
+    def close(self):
+
+        self.fdr.close()
+
+
+mon = Monitor("model/mascot-safety-system.csp")
+mon.run_offline("trace.json")
