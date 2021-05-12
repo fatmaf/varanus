d02237165760563195d4043ed8d354c3aef6d824
Matt Luckcuck <m.luckcuck@gmail.com>
2021-05-12 17:44:12 +0100

    If speed_check now logs times, incrementally, to a csv

--- varanus-python/varanus.py
+++ varanus-python/varanus.py
@@ -4,6 +4,8 @@ import logging
 import argparse
 import os
 import sys
+import csv
+from os.path import exists
 
 ################
 ###CONSTANTS###
@@ -19,6 +21,7 @@ argParser.add_argument("model", help="The location of the model used as the orac
 argParser.add_argument("map", help="The location of the event map", default = "event_map.json")
 argParser.add_argument("type", help="The type of check to be performed", choices=['offline', 'online'])
 argParser.add_argument("-n", "--name", help="The name of the check and therefore name of the log file")
+argParser.add_argument("--log_path", help="The path of the log dir")
 argParser.add_argument("-t", "--trace_file", help="The location of the trace file. Only used if type='offline'")
 argParser.add_argument("-s", "--speed", help="Run 10 timed run and produce the times and mean.).", type=bool, default = False)
 
@@ -33,6 +36,10 @@ if args.name:
     CHECK_NAME = args.name
 else:
     CHECK_NAME = "scenario x"
+if args.log_path:
+    LOG_PATH = args.log_path + "/"
+else:
+    LOG_PATH = "log/"
 #TODO check and warn for incompatible params
 
 ################
@@ -48,11 +55,57 @@ varanus_logger.setLevel(log_level)
 
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-fileHandler = logging.FileHandler("log/varanus-"+logFileName+"-"+time.strftime("%Y_%m_%d-%H:%M:%S")+ ".log")
+
+fileHandler = logging.FileHandler(LOG_PATH + "varanus-"+ TYPE +"-"+logFileName+"-"+time.strftime("%Y_%m_%d-%H:%M:%S")+ ".log")
 
 fileHandler.setFormatter(formatter)
 
 varanus_logger.addHandler(fileHandler)
+
+def run(check_type):
+
+        if check_type == "offline":
+            t0 = time.time()
+            mon = Monitor(MODEL, MAP)
+            mon._run_offline_traces_single(TRACE_FILE)
+        elif check_type == "online":
+            t0 = time.time()
+            mon = Monitor(MODEL, MAP)
+            mon.run_online_traces_accumulate(IP, PORT, timeRun=False)
+
+        #mon.run_online('127.0.0.1', 5044)
+        #mon.run_online_websocket('127.0.0.1', 8080)
+        mon.close()
+        t1 = time.time()
+
+        total = t1-t0
+
+        return total
+
+def log_speed(name, time):
+
+    file_name = "log/speed-check-"+name+".csv"
+
+    #If the file doesn't exist, open and close it -_-
+    if not exists(file_name):
+        output_file = open(file_name , "a")
+        output_file.close()
+
+    #Read the file to the end to get how many runs it has in it
+    output_file = open(file_name , "r")
+    csv_read = csv.reader(output_file, delimiter=',', quotechar='"')
+    run_num = 0
+    for run in csv_read:
+        run_num += 1
+    output_file.close()
+
+    #Write the number of runs +1 into the file, with the time.
+    output_file = open(file_name, "a")
+    csv_write = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+    run_num += 1
+    csv_write.writerow([ "Run" + str(run_num) , time ])
+    output_file.close()
 
 print("+++++++++++++++++++++++")
 print("+++++++ VARANUS +++++++")
@@ -67,73 +120,32 @@ varanus_logger.debug("Varanus Running v" + str(VERSION_NUM))
 
 varanus_logger.info("+++ Testing " + logFileName + " +++")
 
-if SPEED_CHECK == False:
-# NORMAL USAGE
+
 
     #mon.run_offline_rosmon("../rosmon-test/rosmon-mascot-pass.json")
     #mon._run_offline_traces("trace.json")
 
-    if TYPE == "offline":
-        t0 = time.time()
-        mon = Monitor(MODEL, MAP)
-        mon._run_offline_traces_single(TRACE_FILE)
-    elif TYPE == "online":
-        t0 = time.time()
-        mon = Monitor(MODEL, MAP)
-        mon.run_online_traces_accumulate(IP, PORT, timeRun=False)
+##NOW IN RUN
+#    if TYPE == "offline":
+#        t0 = time.time()
+#        mon = Monitor(MODEL, MAP)
+#        mon._run_offline_traces_single(TRACE_FILE)
+#    elif TYPE == "online":
+#        t0 = time.time()
+#        mon = Monitor(MODEL, MAP)
+#        mon.run_online_traces_accumulate(IP, PORT, timeRun=False)
 
     #mon.run_online('127.0.0.1', 5044)
     #mon.run_online_websocket('127.0.0.1', 8080)
-    mon.close()
-    t1 = time.time()
+#    mon.close()
+#    t1 = time.time()
 
-    total = t1-t0
+total = run(TYPE)
 
-    varanus_logger.info("+++ Time: "+ str(total) +"s +++")
+varanus_logger.info("+++ Time: "+ str(total) +"s +++")
 
-    varanus_logger.debug("Varanus Finished")
-elif SPEED_CHECK == True:
-# SPEED CHECK USAGE
-# Like mascot-speed-test.py but inside Varanus
-    varanus_logger.info("+++ SPEED CHECK +++")
-    sys.path.append("../mascot-test/")
-    from mascot_speed_test import write_csv
+varanus_logger.debug("Varanus Finished")
 
-    SOURCE_LIST = ["scenario1-trace", "scenario2-trace", "scenario2a-trace", "scenario2b-trace",
-    "scenario3-trace", "scenario4-trace", "scenario4a-trace", "scenario4b-trace", "scenario5-trace",
-    "scenario6-trace", "scenario7-trace", "scenario0-10-trace", "scenario0-100-trace", "scenario0-1000-trace",
-    "scenario0-10000-trace", "scenario0-100000-trace"]
+if SPEED_CHECK == True:
 
-    times = []
-
-    if TYPE == "offline":
-        OUTPUT_DIR = "../mascot-test/offline-times/"
-
-        #for scenario_name in SOURCE_LIST[0:2]:
-
-        varanus_logger.info("+++ SCENARIO:" + CHECK_NAME + " +++")
-
-        for i in range(10):
-            t0 = time.time()
-
-            mon = Monitor(MODEL, MAP)
-            mon._run_offline_traces_single(TRACE_FILE)
-            mon.close()
-
-            t1 = time.time()
-            total = t1-t0
-            times.append(total)
-
-
-
-        write_csv(CHECK_NAME, times, OUTPUT_DIR)
-
-
-    elif TYPE == "online":
-        OUTPUT_DIR = "../mascot-test/online-times/"
-
-        for scenario_name in SOURCE_LIST[0:2]:
-            print ("SCENARIO:" + scenario_name)
-
-            for i in range(10):
-                mon.run_online_traces_accumulate(IP, PORT, timeRun=True)
+    log_speed(CHECK_NAME, total)
